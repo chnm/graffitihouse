e9c37d3b88bcefaa2a0e22b678b4c089bd4f169a
Jason Heppler <jason.heppler@gmail.com>
2023-02-13 15:49:26 -0600

    Initial thinking through models

---
+++ graffiti/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.urls import reverse 
+
+class Graffiti(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'graffiti_id': self.id})
+
+class GraffitiHasPart(models.Model):
+    graffiti_id = models.ForeignKey(Graffiti, on_delete=models.CASCADE)
+    part_id = models.ForeignKey(Part, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.graffiti_id
+
+class House(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'house_id': self.id})
