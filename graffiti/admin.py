331643b2dda7293b4de842f8ea80d27de7bc944c
Jason Heppler <jason.heppler@gmail.com>
2023-02-27 14:23:37 -0600

    Register models to admin

--- graffiti/admin.py
+++ graffiti/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Graffiti, GraffitiHasPart, House
 
 # Register your models here.
+admin.site.register(Graffiti)
+admin.site.register(GraffitiHasPart)
+admin.site.register(House)
