221dd6e08402cef9be7e83882883eb6ec7631d76
Jason Heppler <jason.heppler@gmail.com>
2023-03-22 15:13:59 -0500

    Added template pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path 
+from django.urls import path
 from .views import HomePageView
+from . import views
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", views.about, name="about"),
+    path("museum/", views.museum, name="museum"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,21 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+
 
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def museum(request):
+    return render(request, "museum.html")
+
+
+handler404 = "pages.views.handler404"
+
+
+def handler404(request, exception):
+    return render(request, "404.html", status=404)
