ebff8b317f5e5229c57af7cdfe89af02976b0469
Jason Heppler <jason.heppler@gmail.com>
2023-04-03 15:33:10 -0500

    Adjustments to user types

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
-    is_staff = models.BooleanField()
-    is_volunteer = models.BooleanField()
-    is_student = models.BooleanField()
-    is_contributor = models.BooleanField()
+    is_staff = models.BooleanField(default=True)
+    is_volunteer = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    is_contributor = models.BooleanField(default=False)
+
 
 class Student(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
 
+
 class Volunteer(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+
 
 class Contributor(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
--- accounts/views.py
+++ accounts/views.py
@@ -2,47 +2,50 @@ from django.contrib.auth import login
 from django.shortcuts import redirect
 from django.views.generic import CreateView
 
-from .forms import StudentSignUpForm, ContributorSignUpForm, VolunteerSignUpForm
+from .forms import ContributorSignUpForm, StudentSignUpForm, VolunteerSignUpForm
 from .models import User
+
 
 class StudentSignUpView(CreateView):
     model = User
     form_class = StudentSignUpForm
-    template_name = 'registration/signup.html'
+    template_name = "registration/signup.html"
 
     def get_context_data(self, **kwargs):
-        kwargs['user_type'] = 'student'
+        kwargs["user_type"] = "student"
         return super().get_context_data(**kwargs)
 
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('students:data_list')
+        return redirect("students:data_list")
+
 
 class ContributorSignUpView(CreateView):
     model = User
     form_class = ContributorSignUpForm
-    template_name = 'registration/signup.html'
+    template_name = "registration/signup.html"
 
     def get_context_data(self, **kwargs):
-        kwargs['user_type'] = 'contributor'
+        kwargs["user_type"] = "contributor"
         return super().get_context_data(**kwargs)
 
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('contributor:data_list')
+        return redirect("contributor:data_list")
 
- class VolunteerSignUpView(CreateView):
+
+class VolunteerSignUpView(CreateView):
     model = User
     form_class = VolunteerSignUpForm
-    template_name = 'registration/signup.html'
+    template_name = "registration/signup.html"
 
     def get_context_data(self, **kwargs):
-        kwargs['user_type'] = 'volunteer'
+        kwargs["user_type"] = "volunteer"
         return super().get_context_data(**kwargs)
 
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('volunteer:data_list')
+        return redirect("volunteer:data_list")
