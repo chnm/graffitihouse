db9932e3dc90be970429283ffa71bacca5b6a55f
Jason Heppler <jason.heppler@gmail.com>
2025-04-10 11:29:24 -0500

    feat: Add overall walls view, individual site view, and individual graffiti item view

--- graffiti/urls.py
+++ graffiti/urls.py
@@ -1,11 +1,12 @@
 # urls.py
 from django.urls import path
 
-from .views import derived_image_detail_view, overall_image_view
+from .views import derived_image_detail_view, list_walls_view, overall_image_view
 
 app_name = "graffiti"
 
 urlpatterns = [
+    path("", list_walls_view, name="walls_list"),
     path("wall/<int:wall_id>/", overall_image_view, name="overall_image"),
     path(
         "derived-image/<int:image_id>/",
--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -6,53 +6,72 @@
  */
 
 module.exports = {
-    content: [
-        /**
-         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
-         */
+  content: [
+    /**
+     * HTML. Paths to Django template files that will contain Tailwind CSS classes.
+     */
 
-        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
-        '../templates/**/*.html',
+    /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
+    "../templates/**/*.html",
 
-        /*
-         * Main templates directory of the project (BASE_DIR/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../templates/**/*.html',
+    /*
+     * Main templates directory of the project (BASE_DIR/templates).
+     * Adjust the following line to match your project structure.
+     */
+    "../../templates/**/*.html",
 
-        /*
-         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../**/templates/**/*.html',
+    /*
+     * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
+     * Adjust the following line to match your project structure.
+     */
+    "../../**/templates/**/*.html",
 
-        /**
-         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
-         * patterns match your project structure.
-         */
-        /* JS 1: Ignore any JavaScript in node_modules folder. */
-        // '!../../**/node_modules',
-        /* JS 2: Process all JavaScript files in the project. */
-        // '../../**/*.js',
+    /**
+     * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
+     * patterns match your project structure.
+     */
+    /* JS 1: Ignore any JavaScript in node_modules folder. */
+    // '!../../**/node_modules',
+    /* JS 2: Process all JavaScript files in the project. */
+    // '../../**/*.js',
 
-        /**
-         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
-         * and make sure the pattern below matches your project structure.
-         */
-        // '../../**/*.py'
-    ],
-    theme: {
-        extend: {},
+    /**
+     * Python: If you use Tailwind CSS classes in Python, uncomment the following line
+     * and make sure the pattern below matches your project structure.
+     */
+    // '../../**/*.py'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cwg: {
+          red: "#d72638",
+          blue: "#256EFF",
+          black: "#2A2D34",
+          white: "#FDFFFC",
+          green: "#7F9172",
+          // Add shades to create a full palette
+          "red-light": "#e35d6c",
+          "red-dark": "#b31e2e",
+          "blue-light": "#5790ff",
+          "blue-dark": "#1e58cc",
+          "black-light": "#444750",
+          "black-dark": "#1e2024",
+          "green-light": "#9baa8f",
+          "green-dark": "#66745b",
+        },
+      },
     },
-    plugins: [
-        /**
-         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-         * for forms. If you don't like it or have own styling for forms,
-         * comment the line below to disable '@tailwindcss/forms'.
-         */
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/line-clamp'),
-        require('@tailwindcss/aspect-ratio'),
-    ],
-}
+  },
+  plugins: [
+    /**
+     * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+     * for forms. If you don't like it or have own styling for forms,
+     * comment the line below to disable '@tailwindcss/forms'.
+     */
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
