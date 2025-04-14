1ebcdd45a2b1e91c047b2a780751bcc6ff7de0c5
Jason Heppler <jason.heppler@gmail.com>
2025-04-14 14:31:15 -0500

    fix: Include static URL in debug

--- config/urls.py
+++ config/urls.py
@@ -16,3 +16,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
